aaf7cb4317ba9878f9adc0030ac53b300f1b5131
Levi Sandoval <l.sandoval@mefasa.org>
2020-10-12 19:33:30 -0700

    ejemplo de archivo de texto

---
+++ U2_ArchivoTxt/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace U2_ArchivoTxt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string archivo = "mocos.txt";
+            using(StreamWriter kk = File.CreateText(archivo)) {
+                kk.WriteLine("come caca");
+                kk.WriteLine("moco seco");
+                kk.WriteLine("hasta aqui llego el archivo");
+            }
+            
+            Console.WriteLine("Fin");
+        }
+    }
+}
