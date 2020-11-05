6dcad6e31395a90eec9b83b6f203ede86512a39e
Levi Sandoval <l.sandoval@mefasa.org>
2020-11-04 19:07:44 -0700

    cargar registro listo

---
+++ U2_CargarRegistro/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace U2_CargarRegistro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string registro = "../U2_RegistroBasico/registro.csv";
+            string registro = "Program.cs";
+            bool existe = File.Exists(registro);
+
+            if(existe) {
+                // Leerlo
+                using(StreamReader contenido = File.OpenText(registro)) {
+                    string linea = "";
+                    while( (linea = contenido.ReadLine()) != null ) {
+                        Console.WriteLine("Linea de texto: {0}", linea);
+                    }
+                }
+            } else {
+                Console.WriteLine("El archivo {0} no existe", registro);
+            }
+        }
+    }
+}
